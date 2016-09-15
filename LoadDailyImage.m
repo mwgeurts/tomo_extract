e1b2c72c9b95e95ab40af2976714c4f43cdf736b
Mark Geurts <mark.w.geurts@gmail.com>
2016-09-15 15:26:40 -0500

    Inverted dimension of IEC-Y registration

--- LoadDailyImage.m
+++ LoadDailyImage.m
@@ -516,7 +516,7 @@ case 'ARCHIVE'
 
         % Store the registration
         image.registration(5) = ...
-            str2double(subnodeList.item(0).getFirstChild.getNodeValue);
+            -str2double(subnodeList.item(0).getFirstChild.getNodeValue);
 
         % Search for registration pitch
         subexpression = xpath.compile(['fullCorrelationDataArray/', ...
